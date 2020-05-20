f4ab2be6e2a950ff4a4f40677591c2bfed978c37
AmrAngry <amr.elghadban@gmail.com>
2020-05-20 16:57:25 +0400

    shuffle function to collection

---
+++ Source/Extensions/Foundation/MutableCollection+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  MutableCollection+Extension.swift
+//  ClockWise
+//
+//  Created by AmrAngry on 5/20/20.
+//
+
+import Foundation
+
+public extension MutableCollection where Index == Int {
+    mutating func shuffle() {
+        if count <= 1 { return }
+        for i in 0..<count - 1 {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            swapAt(i, j)
+        }
+    }
+}
