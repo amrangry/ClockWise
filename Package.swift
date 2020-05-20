c560a7c929f28cb86622c663d26247baf3624c33
AmrAngry <amr.elghadban@gmail.com>
2020-05-20 16:15:12 +0400

    configure SPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ClockWise",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ClockWise",
-            dependencies: []),
+            dependencies: [],
+            path: "Source"),
         .testTarget(
             name: "ClockWiseTests",
             dependencies: ["ClockWise"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
