237e75fd2a7c1c66f2241f93c09b3af0f948a456
AmrAngry <Amr.Elghadban@seddiqiholding.com>
2020-10-13 15:44:40 +0400

    change in signature

--- Source/Extensions/UIKit/UIView+NIB.swift
+++ Source/Extensions/UIKit/UIView+NIB.swift
@@ -2,7 +2,32 @@
 //  UIView+NIB.swift
 //  ClockWise
 //
-//  Created by iSeddiqi Apple on 13/10/2020.
+//  Created by Amr Elghadban (AmrAngry) on 8/3/20.
+//  Copyright © 2020 adka. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public extension UIView {
+    
+    //    static func nib() -> UINib {
+    //        UINib(nibName: self.reusableIdentifier, bundle: Bundle.main)
+    //    }
+    
+    class func nib() -> UINib {
+       func instanceNIB() -> UINib {
+          let nib = UINib(nibName: reusableIdentifier, bundle: nil)
+          return nib
+       }
+       return instanceNIB()
+    }
+    
+    class func initFromNib() -> Self {
+       func instanceFromNib<T: UIView>() -> T {
+          let view = nib().instantiate(withOwner: nil, options: nil).first as! T //swiftlint:disable:this force_cast
+          return view
+       }
+       return instanceFromNib()
+    }
+
+}
