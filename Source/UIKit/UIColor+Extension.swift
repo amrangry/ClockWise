c8ffa9f53feaba78a9eb262fbcdfba4a66132586
Amr Elghadban <amr.elghadban@gmail.com>
2023-05-16 02:32:27 +0400

    color extension

--- Source/UIKit/UIColor+Extension.swift
+++ Source/UIKit/UIColor+Extension.swift
@@ -34,11 +34,16 @@ public extension UIColor {
     }
     
     convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int, alpha: Double) {
         assert(red >= 0 && red <= 255, "Invalid red component")
         assert(green >= 0 && green <= 255, "Invalid green component")
         assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        assert(alpha >= 0 && alpha <= 1, "Invalid alpha component")
         
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha)
     }
     
     convenience init(hex: String) {
