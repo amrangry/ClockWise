eeef4cdfadc015610eeb603775d7d3f6cac13d62
Amr Elghadban <amr.elghadban@gmail.com>
2023-10-26 23:16:41 +0400

    support for Objective C + renaming of method

--- Source/UIKit/UIApplication+Extension.swift
+++ Source/UIKit/UIApplication+Extension.swift
@@ -12,7 +12,7 @@ import UIKit
 public extension UIApplication {
     
     /// you have the key window
-    var currentWindow: UIWindow? {
+    @objc var currentWindow: UIWindow? {
         //        connectedScenes
         //        .filter({$0.activationState == .foregroundActive})
         //        .map({$0 as? UIWindowScene})
@@ -22,33 +22,37 @@ public extension UIApplication {
         
         UIApplication.shared.windows.first(where: { (window) -> Bool in window.isKeyWindow})
     }
-    static func topViewController(controller: UIViewController? = UIApplication.shared.currentWindow?.rootViewController ) -> UIViewController? {
+    
+    @objc
+    static func topVisibleViewController(controller: UIViewController? = UIApplication.shared.currentWindow?.rootViewController ) -> UIViewController? {
         //        let xyz = UIApplication.shared.windows.first?.rootViewController
         //        let xyz2 = UIApplication.shared.keyWindow?.rootViewController
         //        let xyz3 = UIApplication.shared.currentWindow
         
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
         //        if let windowxyz = UIApplication.shared.windows.first(where: { (window) -> Bool in window.isKeyWindow}) {
         //           //your code
         //             // you have the key window
         //            print(windowxyz)
         //        }
+        /* Workaround for Objective c as the default value will not be settled from func signature */
+        let root = controller ?? UIApplication.shared.keyWindow?.rootViewController
+        if let navigationController = root as? UINavigationController {
+            return topVisibleViewController(controller: navigationController.visibleViewController)
         }
-        if let tabController = controller as? UITabBarController {
+        if let tabController = root as? UITabBarController {
             if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
+                return topVisibleViewController(controller: selected)
             }
         }
-        if let presented = controller?.presentedViewController {
-            return topViewController(controller: presented)
+        if let presented = root?.presentedViewController {
+            return topVisibleViewController(controller: presented)
         }
-        return controller
+        return root
     }
     
     static func showPDFFile(fileURL: URL) {
         let documentController = UIDocumentInteractionController(url: fileURL)
-        documentController.delegate = topViewController() as? UIDocumentInteractionControllerDelegate
+        documentController.delegate = topVisibleViewController() as? UIDocumentInteractionControllerDelegate
         documentController.presentPreview(animated: true)
     }
     
